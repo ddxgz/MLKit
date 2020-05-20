7a2d46b700ba1dfaf8ab45ab689cea034eed2745
pcx <ddxgz19880813@gmail.com>
2020-05-20 19:31:26 +0200

    Refact criterioin as protocol

---
+++ Sources/SwiftML/Tree/Criterion.swift
@@ -0,0 +1,102 @@
+import TensorFlow
+
+/// A criterion implementation has a function nodeImpurity that takes input of
+/// groups of labels (either classes or
+/// numerical values) in the form of array of 2 arraies [left, right]
+protocol Criterion {
+    func nodeImpurity(_: [[Float]]) -> Float
+}
+
+struct Gini: Criterion {
+    func nodeImpurity(_ groups: [[Float]]) -> Float {
+        let classes = Set(groups.joined())
+        // print("classes: \(classes)")
+        var N: Float = 0.0
+        for group in groups {
+            N += Float(group.count)
+            // N += Double(group.count)
+        }
+        // print(N)
+        var gini: Float = 0.0
+        for group in groups {
+            let sizeG = Float(group.count)
+            if sizeG == 0 { continue }
+            /// sum((n_k/N)^2) for class k
+            var sumP: Float = 0.0
+            for cls in classes {
+                // let p = group.count(where: $0 == cls) / sizeG
+                let p = Float(group.filter { $0 == cls }.count) / sizeG
+                // print(p)
+                sumP += p * p
+            }
+            gini += (1.0 - sumP) * (sizeG / N)
+        }
+        return Float(gini)
+    }
+}
+
+struct MSE: Criterion {
+    func nodeImpurity(_ groups: [[Float]]) -> Float {
+        var N: Float = 0.0
+        for group in groups {
+            N += Float(group.count)
+        }
+
+        var err: Float = 0
+
+        for group in groups {
+            let sizeG = Float(group.count)
+            if sizeG == 0 { continue }
+            let mean = group.reduce(0, +) / sizeG
+            // // print("group: \(group)")
+            // // print("mean: \(mean)")
+            // // use a tensor fill with mean
+            // let y = Tensor<Float>(repeating: mean, shape: [Int(sizeG), 1])
+            // let yhat = Tensor<Float>(group)
+            // err += meanSquaredErrorTF(y, yhat)
+            let diff = group.map { pow($0 - mean, 2) }
+            err += diff.reduce(0, +) / sizeG
+            // print("err: \(err)")
+        }
+        return err
+    }
+}
+
+struct MSEv1: Criterion {
+    func nodeImpurity(_ groups: [[Float]]) -> Float {
+        var N: Float = 0.0
+        for group in groups {
+            N += Float(group.count)
+        }
+
+        var err: Float = 0
+
+        for group in groups {
+            let sizeG = Float(group.count)
+            if sizeG == 0 { continue }
+            let mean = group.reduce(0, +) / sizeG
+            // print("group: \(group)")
+            // print("mean: \(mean)")
+            // use a tensor fill with mean
+            let y = Tensor<Float>(repeating: mean, shape: [Int(sizeG), 1])
+            let yhat = Tensor<Float>(group)
+            err += meanSquaredErrorTF(y, yhat)
+            // print("err: \(err)")
+        }
+        return err
+    }
+}
+
+func getCriterion(_ type_: String) -> Criterion? {
+    switch type_ {
+    case "gini":
+        return Gini()
+    case "mse":
+        return MSE()
+    case "msev1":
+        return MSEv1()
+    default:
+        print("Criterion: \(type_) not supported!")
+        return nil
+    }
+}
--- Sources/SwiftML/Tree/DecisionTree.swift
+++ Sources/SwiftML/Tree/DecisionTree.swift
@@ -21,91 +21,6 @@ protocol TreeEstimator: Estimator {
     // func score(data x: Tensor<Float>, labels y: Tensor<Float>) -> Float
 }
 
-/// A criterion function takes input of groups of labels (either classes or
-/// numerical values) in the form of array of 2 arraies [left, right]
-typealias CriterionFn = ([[Float]]) -> Float
-
-// func giniImpurity(_ groups: [[Float]], _ classes: [Float]) -> Float {
-func giniImpurity(_ groups: [[Float]]) -> Float {
-    let classes = Set(groups.joined())
-    // print("classes: \(classes)")
-    var N: Float = 0.0
-    for group in groups {
-        N += Float(group.count)
-        // N += Double(group.count)
-    }
-    // print(N)
-    var gini: Float = 0.0
-    for group in groups {
-        let sizeG = Float(group.count)
-        if sizeG == 0 { continue }
-        /// sum((n_k/N)^2) for class k
-        var sumP: Float = 0.0
-        for cls in classes {
-            // let p = group.count(where: $0 == cls) / sizeG
-            let p = Float(group.filter { $0 == cls }.count) / sizeG
-            // print(p)
-            sumP += p * p
-        }
-        gini += (1.0 - sumP) * (sizeG / N)
-    }
-    return Float(gini)
-}
-
-// func mseCriterion(_ groups: [[Float]], _ classes: [Float]) -> Float {
-func mseCriterionV1(_ groups: [[Float]]) -> Float {
-    var N: Float = 0.0
-    for group in groups {
-        N += Float(group.count)
-    }
-
-    var err: Float = 0
-
-    for group in groups {
-        let sizeG = Float(group.count)
-        if sizeG == 0 { continue }
-        let mean = group.reduce(0, +) / sizeG
-        // print("group: \(group)")
-        // print("mean: \(mean)")
-        // use a tensor fill with mean
-        let y = Tensor<Float>(repeating: mean, shape: [Int(sizeG), 1])
-        let yhat = Tensor<Float>(group)
-        err += meanSquaredErrorTF(y, yhat)
-        // print("err: \(err)")
-    }
-    return err
-}
-
-func mseCriterion(_ groups: [[Float]]) -> Float {
-    var N: Float = 0.0
-    for group in groups {
-        N += Float(group.count)
-    }
-
-    var err: Float = 0
-
-    for group in groups {
-        let sizeG = Float(group.count)
-        if sizeG == 0 { continue }
-        let mean = group.reduce(0, +) / sizeG
-        // // print("group: \(group)")
-        // // print("mean: \(mean)")
-        // // use a tensor fill with mean
-        // let y = Tensor<Float>(repeating: mean, shape: [Int(sizeG), 1])
-        // let yhat = Tensor<Float>(group)
-        // err += meanSquaredErrorTF(y, yhat)
-        let diff = group.map { pow($0 - mean, 2) }
-        err += diff.reduce(0, +) / sizeG
-        // print("err: \(err)")
-    }
-    return err
-}
-
-/// can use a dict for now, change to function with switch for more complcated cases
-let Criteria = [
-    "gini": giniImpurity,
-    "mse": mseCriterion,
-]
 
 typealias Groups = (left: [Int], right: [Int])
 typealias LabelGroups = (left: [Float], right: [Float])
@@ -269,7 +184,8 @@ func notConstant(_ data: Matrix) -> Bool {
 }
 
 struct BestFirstTreeBuilder {
-    let criterion: CriterionFn
+    // let criterion: CriterionFn
+    let criterion: Criterion
     let isClassification: Bool
     var nFeatures: Int
     var classes: [Int]
@@ -411,7 +327,8 @@ struct BestFirstTreeBuilder {
             let sampleSplit = (left, right)
 
             let labelGroups = getLabelGroups(sampleSplit: sampleSplit, data: data)
-            let score = criterion([labelGroups.left, labelGroups.right])
+            // let score = criterion([labelGroups.left, labelGroups.right])
+            let score = criterion.nodeImpurity([labelGroups.left, labelGroups.right])
 
             let node = Node(depth: depth, feature: -1, splitValue: bstSplitValue, score: score,
                             groups: sampleSplit)
@@ -455,7 +372,8 @@ struct BestFirstTreeBuilder {
                 let labelGroups = getLabelGroups(sampleSplit: sampleSplit, data: data)
 
                 // print("labelGroups: \(labelGroups)")
-                let score = criterion([labelGroups.left, labelGroups.right])
+                // let score = criterion([labelGroups.left, labelGroups.right])
+                let score = criterion.nodeImpurity([labelGroups.left, labelGroups.right])
 
                 // let score = criterion([labelLeft, labelRight])
 
@@ -534,7 +452,8 @@ struct BestFirstTreeBuilder {
 }
 
 struct DecisionTree: TreeEstimator {
-    var criterion, splitter: String
+    // var criterion, splitter: String
+    var criterion: String
     var nFeatures: Int = 0
     var maxDepth: Int
     var maxFeatures: Int
@@ -548,12 +467,14 @@ struct DecisionTree: TreeEstimator {
     var featureImportances: Tensor<Float> { return Tensor(0) }
     var scoring: String = "accuracy"
 
-    init(criterion: String = "gini", splitter: String = "best",
+    //  splitter: String = "best",
+    init(criterion: String = "gini",
          maxDepth: Int = -1,
          maxFeatures: Int = -1, minSamplesSplit: Int = 2, minSamplesLeaf: Int = 1,
          minImpurityDecrease: Float = 0,
          scoring: String = "accuracy") {
-        (self.criterion, self.splitter) = (criterion, splitter)
+        // (self.criterion, self.splitter) = (criterion, splitter)
+        self.criterion = criterion
         self.maxDepth = maxDepth
         self.maxFeatures = maxFeatures
         self.minSamplesSplit = minSamplesSplit
@@ -615,7 +536,11 @@ struct DecisionTree: TreeEstimator {
             maxFeatures = nFeatures
         }
 
-        guard let fnCriterion = Criteria[criterion] else {
+        // guard let fnCriterion = Criteria[criterion] else {
+        //     print("criterion not found")
+        //     return
+        // }
+        guard let fnCriterion = getCriterion(criterion) else {
             print("criterion not found")
             return
         }
