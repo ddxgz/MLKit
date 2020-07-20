da50d69c58f3f0bc85cf70fe903b05528bbc0644
pcx <ddxgz19880813@gmail.com>
2020-07-20 22:12:19 +0200

    Add LASwift dep

--- Package.swift
+++ Package.swift
@@ -14,17 +14,20 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/alexandertar/LASwift", from: "0.2.3"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
+        // .package(name: "XGBoostSwift", url: "https://github.com/ddxgz/XGBoost.swift.git", from: "0.3.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftML",
-            dependencies: []
+            dependencies: [.product(name: "Logging", package: "swift-log")]
         ),
-        // .target(
-        //     name: "run",
-        //     dependencies: ["SwiftML"]
-        // ),
+        .testTarget(
+            name: "SwiftMLTests",
+            dependencies: ["SwiftML", "LASwift"]
+        ),
     ]
 )
